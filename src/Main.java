1ac550a25d841cfa630f05a73aaa81464198e075
KukoBerry <111517844+KukoBerry@users.noreply.github.com>
2024-08-28 12:59:19 -0300

    fix: input aberto

--- src/Main.java
+++ src/Main.java
@@ -11,39 +11,40 @@ public class Main {
         sistema.toggleDebug();
         repo.carregarDadosDeTeste();
 
-        Scanner input = new Scanner(System.in);
-        String inputLine, comando;
-        String[] argumentos;
+        try (Scanner input = new Scanner(System.in)) {
+            String inputLine, comando;
+            String[] argumentos;
 
-        if (sistema.isDebugging()){
-            sistema.executarComando("obs", new String[]{"100", "100"});
-            sistema.executarComando("emp", new String[]{"123", "100"});
-            sistema.executarComando("emp", new String[]{"456", "100"});
-            sistema.executarComando("liv", new String[]{"100"});
-            sistema.executarComando("usu", new String[]{"123"});
-            sistema.executarComando("usu", new String[]{"456"});
-            sistema.executarComando("ntf", new String[]{"100"});
-        }
+            if (sistema.isDebugging()){
+                sistema.executarComando("obs", new String[]{"100", "100"});
+                sistema.executarComando("emp", new String[]{"123", "100"});
+                sistema.executarComando("emp", new String[]{"456", "100"});
+                sistema.executarComando("liv", new String[]{"100"});
+                sistema.executarComando("usu", new String[]{"123"});
+                sistema.executarComando("usu", new String[]{"456"});
+                sistema.executarComando("ntf", new String[]{"100"});
+            }
 
-        Logger.logInfo("Sistema de Biblioteca");
-        Logger.logInfo("Grupo: David Lima e João Soares");
-        boolean running = true;
-        while (running){
-            System.out.print(" > ");
-            inputLine = input.nextLine();
-            comando = inputLine.substring(0,3);
-            Logger.logDebug("Comando: " + comando);
-            if (comando.equals("sai")){
-                running = false;
-                Logger.logAviso("Encerrado sistema.");
-                continue;
+            Logger.logInfo("Sistema de Biblioteca");
+            Logger.logInfo("Grupo: David Lima e João Soares");
+            boolean running = true;
+            while (running){
+                System.out.print(" > ");
+                inputLine = input.nextLine();
+                comando = inputLine.substring(0,3);
+                Logger.logDebug("Comando: " + comando);
+                if (comando.equals("sai")){
+                    running = false;
+                    Logger.logAviso("Encerrado sistema.");
+                    continue;
+                }
+                argumentos = inputLine.substring(4).split(" ");
+                Logger.logDebug("Argumentos: ");
+                for (int i = 0; sistema.isDebugging() && i < argumentos.length; i++){
+                    Logger.logDebug("  "+ (i+1) + ". " + argumentos[i]);
+                }
+                sistema.executarComando(comando, argumentos);
             }
-            argumentos = inputLine.substring(4).split(" ");
-            Logger.logDebug("Argumentos: ");
-            for (int i = 0; sistema.isDebugging() && i < argumentos.length; i++){
-                Logger.logDebug("  "+ (i+1) + ". " + argumentos[i]);
-            }
-            sistema.executarComando(comando, argumentos);
         }
     }
 }
